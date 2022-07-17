7fa748f632c36180b39291d95e57f2b8dfd16edc
Ilyar <761285+ilyar@users.noreply.github.com>
2022-07-17 09:12:07 +0200

    fix dependency

--- src/ui/util.ts
+++ src/ui/util.ts
@@ -6,10 +6,10 @@ export function avatarsRender(user: string, algorithm = 'pixel-art') {
     return `<img class="card-img-top" alt="${user}" src="${dicebear(user, algorithm)}">`
 }
 
-import {Modal} from 'bootstrap'
+import * as bootstrap from 'bootstrap'
 
 export function loginModalHide() {
     const loginModal = document.getElementById('loginModal')
-    const modal = Modal.getInstance(loginModal)
+    const modal = bootstrap.Modal.getInstance(loginModal)
     modal.hide()
 }
